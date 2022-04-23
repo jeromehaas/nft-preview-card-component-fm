58707c1f5b61f3577d8410b9bc52aa770a7cdb60
Jerome Haas <hello@jeromehaas.dev>
2022-04-23 17:11:42 +0200

    Setup: add artwork scene

--- src/js/index.js
+++ src/js/index.js
@@ -1,55 +1,8 @@
-import lottieWeb from 'lottie-web';
+import Card from './Card';
+import Artwork from './Artwork';
 
-class Card {
+const card = new Card();
+card.init();
 
-	constructor() {
-		this.name = 'nft-card';
-		this.elements = {
-			preview: {
-				banner: document.querySelector('.card__preview .preview__banner'),
-				eye: document.querySelector('.card__preview .preview__eye'),
-			}
-		};
-		this.lotties = {
-			eye: {
-				container: document.querySelector('.card__preview .preview__eye'),
-				element: null
-			}
-		};
-		this.init();
-	}
-
-	init = () => {
-		this.addEventListener();
-		this.createEye();
-	};
-
-	createEye = () => {
-		this.lotties.eye.element = lottieWeb.loadAnimation({
-			container: this.lotties.eye.container,
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			path: '/assets/lotties/lottie-eye.json'
-		});
-		this.lotties.eye.element.setSpeed(0.5);
-	};
-
-	addEventListener = () => {
-		this.elements.preview.banner.addEventListener('mouseenter', () => this.showBanner());
-		this.elements.preview.banner.addEventListener('mouseleave', () => this.hideBanner());
-	};
-
-	showBanner = () => {
-		this.elements.preview.banner.style.opacity = 0.5;
-		this.elements.preview.eye.style.opacity = 1;
-	};
-
-	hideBanner = () => {
-		this.elements.preview.banner.style.opacity = 0;
-		this.elements.preview.eye.style.opacity = 0;
-	};
-
-}
-
-new Card();
+const artwork = new Artwork();
+artwork.init();
